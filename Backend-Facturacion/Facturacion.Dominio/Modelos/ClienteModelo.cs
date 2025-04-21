4387d4f5c8e9e61191b8c01e9bc72c335591d2e9
BM <brianalfredomr@gmail.com>
2025-04-20 22:28:02 -0500

    req. ajustes en respuesta al consultar clientes

--- Backend-Facturacion/Facturacion.Dominio/Modelos/ClienteModelo.cs
+++ Backend-Facturacion/Facturacion.Dominio/Modelos/ClienteModelo.cs
@@ -8,5 +8,6 @@
         public string Telefono { get; set; }
         public string Correo { get; set; }
         public string Direccion { get; set; }
+        public bool Estado { get; set; }
     }
 }
